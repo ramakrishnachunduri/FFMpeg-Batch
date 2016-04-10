063398717913882a290d91a5cc31ef64fae48f8e
Rama Krishna Chuduri <ramakrishnachunduri@gmail.com>
2016-04-11 03:47:41 +0530

    Video with h264
    Committing scripts that allow to compress videos in a directory to mp4
    using h264 codec

---
+++ Windows/h264/video_mov_to_h264_mp4.bat
@@ -0,0 +1,1 @@
+for /f "tokens=*" %%a in ('dir /b /s *.mov') do ffmpeg -i "%%a" -vcodec libx264 -level 21 -b 640k -coder 1 -f psp -flags +loop -trellis 2 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -g 250 "%%~na.mp4"
