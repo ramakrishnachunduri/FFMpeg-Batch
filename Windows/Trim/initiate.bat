db6ce2389deecd6369ea35ed2d5a45c363a072bd
Rama Krishna Chuduri <ramakrishnachunduri@gmail.com>
2017-02-19 11:48:40 +0530

    Trim Videos at End
    - Trim set of video files that has few seconds of promotions at end.

---
+++ Windows/Trim/initiate.bat
@@ -0,0 +1,3 @@
+for %%i in (*.mp4) do (
+call trim_file.bat "%%i"
+)
---
+++ Windows/Trim/trim_file.bat
@@ -0,0 +1,10 @@
+for /f "tokens=*" %%a in ('ffprobe -show_format -i %1 ^| find "duration"') do set _duration=%%a
+set _duration=%_duration:~9%
+for /f "delims=. tokens=1*" %%b in ('echo %_duration%') do set /a "_durS=%%b"
+for /f "delims=. tokens=2*" %%c in ('echo %_duration%') do set "_durMS=%%c"
+rem following line is seconds to cut
+set /a "_durS-=30"
+set "_newduration=%_durS%.%_durMS%"
+set "_output=%~n1"
+md trimmed
+ffmpeg -ss 0 -i %1 -t %_newduration% -c copy "trimmed\%_output%.mp4"
