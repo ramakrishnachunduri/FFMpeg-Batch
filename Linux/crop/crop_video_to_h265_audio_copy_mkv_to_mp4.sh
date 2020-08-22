e1d8391eeb2b9e18a6648213a3ff96cffaf49024
Rama Krishna Chunduri <ramakrishnachunduri@gmail.com>
2020-08-23 02:29:18 +0530

    Linux Scripts - Croping
    - Scripts for mkv,m4v files to crop video and encode to h265 while leaving audio as is
    
    - Scripts for mkv,m4v files to crop video and encode to h265 and encode audio to aac
    
    (name of directory in which files exist must be height of letterbox to be croped, Ex: files in folder named 10 shall be croped 10px in top and bottom)

---
+++ Linux/crop/crop_video_to_h265_audio_copy_mkv_to_mp4.sh
@@ -0,0 +1,9 @@
+f="$(basename -- $PWD)";
+d=$(( f * 2 ));
+for fname in *.mkv **/*.mkv **/**/*.mkv; do
+    [ -f "$fname" ] || continue
+    printf '%s\n' "processing file is $fname"
+    ffmpeg -i "$fname" -map_metadata -1 -acodec copy -vcodec libx265 -filter:v "crop=in_w:in_h-$d" "${fname%%.*}.mp4"
+done
+
+
---
+++ Linux/crop/crop_video_to_h265_audio_to_aac_m4v_to_mp4.sh
@@ -0,0 +1,7 @@
+f="$(basename -- $PWD)";
+d=$(( f * 2 ));
+for fname in *.m4v **/*.m4v **/**/*.m4v; do
+    [ -f "$fname" ] || continue
+    printf '%s\n' "processing file is $fname"
+    ffmpeg -i "$fname" -map_metadata -1 -acodec aac -vcodec libx265 -filter:v "crop=in_w:in_h-$d" "${fname%%.*}.mp4"
+done
